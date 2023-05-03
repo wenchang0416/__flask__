a194a1ebad29d35610e9307b3cff35e349111726
張雯嬌(emily) <wenchang0416@gmail.com>
2023-05-03 15:27:15 +0800

    modify

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+@app.route("/")
+def index():
+    #return "Hello Flask"
+    return render_template("index.jinja.html")
+
+@app.route("/learning/")
+def learning():
+    return render_template("learning.jinja.html")
