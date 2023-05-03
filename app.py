b7d54b0e205c43dc182b41ce1f4c8e3704d2308c
張雯嬌(emily) <wenchang0416@gmail.com>
2023-05-03 15:43:17 +0800

    modify

--- app.py
+++ app.py
@@ -3,9 +3,6 @@ from flask import Flask,render_template
 app = Flask(__name__)
 @app.route("/")
 def index():
-    #return "Hello Flask"
-    return render_template("index.jinja.html")
-
-@app.route("/learning/")
-def learning():
     return render_template("learning.jinja.html")
+
+
